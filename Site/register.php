71f346fb58cffd8561d310f4714789766edb80fd
Bangouboy <37013716+Bangouboy@users.noreply.github.com>
2018-03-19 12:46:52 +0100

    2nd envoi du site - avec erreur sur inscription

--- Site/register.php
+++ Site/register.php
@@ -2,9 +2,10 @@
 if(isset($_POST['testEnvoi'])){
 	if ((isset($_POST['nom']) && !empty($_POST['nom']))
 				&& (isset($_POST['prenom'])) && !empty($_POST['prenom'])
-	            && (isset($_POST['birthday'])
-	            && (isset($_POST['email'])
-	            && (isset($_POST['password']))))){
+	            && (isset($_POST['birthday']) && !empty($_POST['birthday'])
+	            && (isset($_POST['email']) && !empty($_POST['email'])
+	            && (isset($_POST['password']) && !empty($_POST['password'])
+	                )))){
 		extract($_POST);
 
 			try{
