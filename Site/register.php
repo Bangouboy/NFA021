dcc770f0b8f6f326ebc593ad1dc51f744dcd8dd3
Bangouboy <37013716+Bangouboy@users.noreply.github.com>
2018-03-19 09:34:24 +0100

    Chargement NFA021 avec site + Inscription

---
+++ Site/register.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_POST['testEnvoi'])){
+	if ((isset($_POST['nom']) && !empty($_POST['nom']))
+				&& (isset($_POST['prenom'])) && !empty($_POST['prenom'])
+	            && (isset($_POST['birthday'])
+	            && (isset($_POST['email'])
+	            && (isset($_POST['password']))))){
+		extract($_POST);
+
+			try{
+				$bdd=new PDO('mysql:host=localhost;dbname=ateliernfa021', 'root', '');
+				$reponse = $bdd->query("INSERT INTO utilisateur VALUES(default,'$nom','$prenom','$birthday','$email','$password')")or exit(print_r($bdd->errorInfo()));
+				echo "Inscription OK";
+				}
+				
+			catch (PDOException $e){
+				die('Erreur : ' .$e->getMessage());
+			}
+			
+
+		}
+	}
+
+
+?>
